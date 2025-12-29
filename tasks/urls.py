25673239b22e562cccd04b99a669f34dcf5f7961
RafE-07 <mahmudrezarafi@gmail.com>
2025-12-29 20:22:26 +0600

    Update task views and templates

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,8 +5,9 @@ from . import views
 app_name = 'tasks'
 
 urlpatterns = [
-    path('', views.home, name='home'),
     path('task_list/', views.task_list, name='task_list'),
     path('add_task/', views.add_task, name='add_task'),
-   
+    path('delete_task/<int:id>/', views.delete_task, name='delete_task'),
+    path('edit_task/<int:id>/', views.edit_task, name='edit_task'),
+    path('toggle_task/<int:id>/', views.toggle_task, name='toggle_task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,14 +1,40 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+
 from .models import Task
 
+
+# Create your views here.
 def task_list(request):
-    task=Task.objects.all()
-    context = {'task': task}
-    return render(request, 'tasks/task_list.html', context)
+    tasks = Task.objects.all()
+    context = {"tasks": tasks}
+    return render(request, "tasks/task_list.html", context)
+
 
 def add_task(request):
-    return HttpResponse("Add Task View")
+    if request.method == "POST":
+        title = request.POST.get("title", "")
+        Task.objects.create(title=title)
+    return redirect("tasks:task_list")
 
-def home(request):
-    return render(request, 'tasks/base.html')
+
+def edit_task(request, id):
+    task = get_object_or_404(Task, id=id)
+    if request.method == "POST":
+        title = request.POST.get("title", "")
+        task.title = title
+        task.save()
+        return redirect("tasks:task_list")
+    return render(request, "tasks/task_edit.html", {"task": task})
+
+
+def toggle_task(request, id):
+    task = get_object_or_404(Task, id=id)
+    task.completed = not task.completed
+    task.save()
+    return redirect("tasks:task_list")
+
+
+def delete_task(request, id):
+    task = get_object_or_404(Task, id=id)
+    task.delete()
+    return redirect("tasks:task_list")
